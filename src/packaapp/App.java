f42b4efb2caf8187de58a788259b708bddee75e2
Iura Zelujec <iur_zel_req@goierrieskola.org>
2026-02-10 11:21:11 +0100

    App klasea hasiera

---
+++ src/packaapp/App.java
@@ -0,0 +1,19 @@
+package packaapp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import packconexion.Conexion;
+
+public class App {
+    private static Connection conn;
+    private static String usuarioActual;
+    private static String rolActual;
+    private static int langileId;
+    
+    public static void main(String[] args) {
+        
+    } 
+    
+    
+}
